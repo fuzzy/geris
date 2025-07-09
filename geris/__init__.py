621b8fb27504425dc6842230521e89603d4171d1
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2025-07-09 16:35:56 -0700

    fix: cleaned up the packaging

--- geris/__init__.py
+++ geris/__init__.py
@@ -2,6 +2,7 @@
 
 # stdlib
 import os
+import sys
 import json
 import time
 import inspect
@@ -533,57 +534,59 @@ class AiChatApp(App):
 def main():
     global debugFlag, config
 
-    if __name__ == "__main__":
-        parser = argparse.ArgumentParser(
-            prog="geris",
-            description="Gitea issue management with a sprinkling of Eris.",
-            epilog="Fnord!",
-        )
-        parser.add_argument(
-            "-c",
-            "--config",
-            type=str,
-            help="Specify the config file to use.",
-            default="~/.gerisrc",
-        )
-        parser.add_argument(
-            "-d",
-            "--debug",
-            action="store_true",
-            help="Show debugging window and log tool usage",
-        )
-        parser.add_argument(
-            "-g",
-            "--gitea-profile",
-            type=str,
-            help="Specify the gitea profile to use.",
-            default="default",
-        )
-        parser.add_argument(
-            "-o",
-            "--openai-profile",
-            type=str,
-            help="Specify the openai profile to use.",
-            default="default",
-        )
+    parser = argparse.ArgumentParser(
+        prog="geris",
+        description="Gitea issue management with a sprinkling of Eris.",
+        epilog="Fnord!",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        help="Specify the config file to use.",
+        default=f"{os.getenv('HOME')}/.gerisrc",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Show debugging window and log tool usage",
+    )
+    parser.add_argument(
+        "-g",
+        "--gitea-profile",
+        type=str,
+        help="Specify the gitea profile to use.",
+        default="default",
+    )
+    parser.add_argument(
+        "-o",
+        "--openai-profile",
+        type=str,
+        help="Specify the openai profile to use.",
+        default="default",
+    )
 
-        args = parser.parse_args()
-        config.read(args.config)
+    args = parser.parse_args()
+    if not os.path.isfile(args.config):
+        print(f"\033[1;31mERROR\033[0m: {args.config} does not exist.")
+        sys.exit(1)
+    config.read(args.config)
 
-        if args.debug:
-            debugFlag = True
+    if args.debug:
+        debugFlag = True
 
-        openaiConfig = config[f"openai:{args.openai_profile}"]
-        openai.api_base = openaiConfig.get("uri", "UNSET")
-        openai.api_key = openaiConfig.get("token", "UNSET")
+    openaiConfig = config[f"openai:{args.openai_profile}"]
+    openai.api_base = openaiConfig.get("uri", "UNSET")
+    openai.api_key = openaiConfig.get("token", "UNSET")
 
-        app = AiChatApp()
-        app.setup_app(
-            config[f"gitea:{args.gitea_profile}"].get("uri", "UNSET"),
-            config[f"gitea:{args.gitea_profile}"].get("token", "UNSET"),
-            config[f"openai:{args.openai_profile}"].get("model", "UNSET"),
-        )
-        app.run()
+    app = AiChatApp()
+    app.setup_app(
+        config[f"gitea:{args.gitea_profile}"].get("uri", "UNSET"),
+        config[f"gitea:{args.gitea_profile}"].get("token", "UNSET"),
+        config[f"openai:{args.openai_profile}"].get("model", "UNSET"),
+    )
+    app.run()
 
 
 if __name__ == "__main__":
